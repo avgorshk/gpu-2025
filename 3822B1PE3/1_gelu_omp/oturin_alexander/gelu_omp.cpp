66d5eabbface3d9eda13102f2f79fbc0fddefe0c
ovVrLFg8ks <oturin.alexandr@gmail.com>
2025-09-23 23:50:05 +0300

    optimizations

--- 3822B1PE3/1_gelu_omp/oturin_alexander/gelu_omp.cpp
+++ 3822B1PE3/1_gelu_omp/oturin_alexander/gelu_omp.cpp
@@ -3,12 +3,14 @@
 std::vector<float> GeluOMP(const std::vector<float>& input) {
   std::vector<float> output(input.size());
 
+  float sqrt_2_pi = std::sqrt(2 / std::numbers::pi);
+
 #pragma omp parallel for
   for (size_t i = 0; i < input.size(); i++) {
     float x = input[i];
-    output[i] = 0.5 * x *
-                (1 + std::tanh(std::sqrt(2 / std::numbers::pi) *
-                               (x + 0.044715 * std::pow(x, 3))));
+    float x_cubed = x * x * x;
+    output[i] =
+        0.5f * x * (1.0f + std::tanh(sqrt_2_pi * (x + 0.044715f * x_cubed)));
   }
 
   return output;
