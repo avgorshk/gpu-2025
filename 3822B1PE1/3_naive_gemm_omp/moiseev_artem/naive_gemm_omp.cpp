dce5b0ac249f722c77e57d1e3efab840ef0e821c
Artyom <amoiseev_00@mail.ru>
2025-09-30 21:13:58 +0300

    task3

---
+++ 3822B1PE1/3_naive_gemm_omp/moiseev_artem/naive_gemm_omp.cpp
@@ -0,0 +1,25 @@
+#include "naive_gemm_omp.h"
+#include <vector>
+#include <omp.h>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+
+    std::vector<float> c(n * n, 0.0f);
+
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            const float a_ik = a[i * n + k];
+            
+            #pragma omp simd
+            for (int j = 0; j < n; ++j) {
+                c[i * n + j] += a_ik * b[k * n + j];
+            }
+        }
+    }
+
+    return c;
+}
