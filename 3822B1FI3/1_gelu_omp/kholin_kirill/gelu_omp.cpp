e377fa22407c37c9e7e1befb6cc00eea11864512
Kirius257 <holin.kirill@yandex.ru>
2025-10-02 16:38:28 +0300

    fix formula

--- 3822B1FI3/1_gelu_omp/kholin_kirill/gelu_omp.cpp
+++ 3822B1FI3/1_gelu_omp/kholin_kirill/gelu_omp.cpp
@@ -16,8 +16,8 @@ std::vector<float> GeluOMP(const std::vector<float> &input) {
     float x = input[i];
     float x_cube = x * x * x;
     float arg_tanh = sqrt_coeff * (x + special_coeff * x_cube);
-    float exp_func = two_coeff / (expf(negative_two_coeff * arg_tanh));
-    output[i] = half_coeff * x * (one_coeff + exp_func);
+    float exp_func = two_coeff / (one_coeff + expf(negative_two_coeff * arg_tanh));
+    output[i] = half_coeff * x * exp_func;
   }
   return output;
 }
