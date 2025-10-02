0c96a162decabb33eca2298e0af4b98a47ac0d9a
Kirius257 <holin.kirill@yandex.ru>
2025-10-02 16:28:25 +0300

    1_gelu_omp

---
+++ 3822B1FI3/1_gelu_omp/kholin_kirill/gelu_omp.cpp
@@ -0,0 +1,23 @@
+#include "gelu_omp.h"
+
+constexpr float sqrt_coeff = 0.7978845608f;
+constexpr float half_coeff = 0.5f;
+constexpr float two_coeff = 2.0f;
+constexpr float one_coeff = 1.0f;
+constexpr float special_coeff = 0.044715f;
+constexpr float negative_two_coeff = -2.0f;
+
+std::vector<float> GeluOMP(const std::vector<float> &input) {
+  size_t size_data = input.size();
+  std::vector<float> output(size_data);
+
+#pragma omp parallel for
+  for (size_t i = 0; i < size_data; i++) {
+    float x = input[i];
+    float x_cube = x * x * x;
+    float arg_tanh = sqrt_coeff * (x + special_coeff * x_cube);
+    float exp_func = two_coeff / (expf(negative_two_coeff * arg_tanh));
+    output[i] = half_coeff * x * (one_coeff + exp_func);
+  }
+  return output;
+}
