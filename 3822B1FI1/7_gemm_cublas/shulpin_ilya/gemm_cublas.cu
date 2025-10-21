058141685108624fc92955d2c4adfbf17c5da13f
NeeMiyuki <zaxarov2706@gmail.com>
2025-10-22 01:42:11 +0300

    first commit

---
+++ 3822B1FI1/7_gemm_cublas/shulpin_ilya/gemm_cublas.cu
@@ -0,0 +1,45 @@
+#include "gemm_cublas.h"
+
+std::vector<float> GemmCUBLAS(const std::vector<float>& a,
+                              const std::vector<float>& b,
+                              int n) {
+    size_t N = static_cast<size_t>(n);
+
+    std::vector C(N * N, 0.0f);
+
+    const size_t bytes = N * N * sizeof(float);
+
+    float* D_a = nullptr, * D_b = nullptr, * D_c = nullptr;
+
+    const float alpha = 1.0f, beta = 0.0f;
+
+    cudaMalloc(&D_a, bytes);
+    cudaMalloc(&D_b, bytes);
+    cudaMalloc(&D_c, bytes);
+
+    cudaMemcpy(D_a, a.data(), bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(D_b, b.data(), bytes, cudaMemcpyHostToDevice);
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    cublasSgemm(handle,
+        CUBLAS_OP_N,
+        CUBLAS_OP_N,
+        n, n, n,
+        &alpha,
+        D_a, n,
+        D_b, n,
+        &beta,
+        D_c, n);
+
+    cudaMemcpy(C.data(), D_c, bytes, cudaMemcpyDeviceToHost);
+
+    cublasDestroy(handle);
+
+    cudaFree(D_a);
+    cudaFree(D_b);
+    cudaFree(D_c);
+
+    return C;
+}
