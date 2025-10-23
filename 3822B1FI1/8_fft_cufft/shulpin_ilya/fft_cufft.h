3bf0a95766b522c9e8fdcf388621be1d9694945d
NeeMiyuki <zaxarov2706@gmail.com>
2025-10-23 17:46:26 +0300

    first commit

---
+++ 3822B1FI1/8_fft_cufft/shulpin_ilya/fft_cufft.h
@@ -0,0 +1,10 @@
+#ifndef __FFT_CUFFT_H
+#define __FFT_CUFFT_H
+
+#include <cuda_runtime.h>
+#include <cufft.h>
+#include <vector>
+
+std::vector<float> FffCUFFT(const std::vector<float>& input, int batch);
+
+#endif // __FFT_CUFFT_H
