a1c33e56a0e9fef53824c593f4f6a04e07f44cc6
MelozRu <61163648+MelozRu@users.noreply.github.com>
2025-10-25 16:58:35 +0300

    Task 9 Fix

--- 3822B1PE4/9_gelu_ocl/podovinnikov_artyom/gelu_ocl.h
+++ 3822B1PE4/9_gelu_ocl/podovinnikov_artyom/gelu_ocl.h
@@ -1,6 +1,11 @@
 #ifndef __GELU_OCL_H
 #define __GELU_OCL_H
 
+#include <CL/cl.h>
+
+#include <cmath>
+#include <cstring>
+#include <string>
 #include <vector>
 
 std::vector<float> GeluOCL(const std::vector<float>& input, int platform);
