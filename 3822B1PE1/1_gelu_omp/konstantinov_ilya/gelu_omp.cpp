641d0def5205b8c2f58a2be157dcd3777f341299
илья константинов <ilyukon2020@gmail.com>
2025-10-26 19:47:43 +0300

    изменение 2

--- 3822B1PE1/1_gelu_omp/konstantinov_ilya/gelu_omp.cpp
+++ 3822B1PE1/1_gelu_omp/konstantinov_ilya/gelu_omp.cpp
@@ -1,32 +1,19 @@
 #include "gelu_omp.h"
 #include <cmath>
 #include <vector>
-
-#ifdef _OPENMP
 #include <omp.h>
-#endif
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
-  const size_t size = input.size();
-  std::vector<float> output(size);
-
-  if (size == 0) {
-    return output;
-  }
+  std::vector<float> output(input.size());
 
   constexpr float alpha = 0.044715f;
   constexpr float beta = 0.7978845608028654f;
 
-#ifdef _OPENMP
 #pragma omp parallel for
-#endif
-  for (size_t i = 0; i < size; ++i) {
-    const float x = input[i];
-    const float x_cube = x * x * x;
-    const float inner = beta * (x + alpha * x_cube);
-    const float exp_val = std::exp(-2.0f * inner);
-    const float tanh_approx = 1.0f - 2.0f / (1.0f + exp_val);
-    output[i] = 0.5f * x * (1.0f + tanh_approx);
+  for (size_t i = 0; i < input.size(); ++i) {
+    float x = input[i];
+    float inner = beta * (x + alpha * x * x * x);
+    output[i] = 0.5f * x * (1.0f + std::tanh(inner));
   }
 
   return output;
