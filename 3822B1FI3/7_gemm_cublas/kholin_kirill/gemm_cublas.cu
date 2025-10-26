eae64cd98d671fb2013c46e3c0f87aee2b2f8c86
Kirius257 <holin.kirill@yandex.ru>
2025-10-26 16:15:36 +0300

    -remove exeptions, +add sync

--- 3822B1FI3/7_gemm_cublas/kholin_kirill/gemm_cublas.cu
+++ 3822B1FI3/7_gemm_cublas/kholin_kirill/gemm_cublas.cu
@@ -1,44 +1,20 @@
 #include "gemm_cublas.h"
 #include <cublas_v2.h>
-#include <cuda_runtime.h>
-#include <stdexcept>
 
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
                               const std::vector<float>& b,
                               int n) {
-    if (n <= 0) {
-        throw std::invalid_argument("Matrix size n must be positive");
-    }
-    const size_t matrix_size = n * n;
-    if (a.size() != matrix_size || b.size() != matrix_size) {
-        throw std::invalid_argument("Matrix sizes do not match n*n");
-    }
+    std::vector<float> c(n * n);
 
-    const size_t bytes = matrix_size * sizeof(float);
-    
+    float *d_a;
+    float *d_b;
+    float *d_c;
 
-    std::vector<float> c(matrix_size);
+    size_t bytes = n * n * sizeof(float);
 
-    float* d_a = nullptr;
-    float* d_b = nullptr; 
-    float* d_c = nullptr;
-    
-    cudaError_t cuda_status;
-    cuda_status = cudaMalloc(&d_a, bytes);
-    if (cuda_status != cudaSuccess) throw std::runtime_error("cudaMalloc failed for d_a");
-    
-    cuda_status = cudaMalloc(&d_b, bytes);
-    if (cuda_status != cudaSuccess) {
-        cudaFree(d_a);
-        throw std::runtime_error("cudaMalloc failed for d_b");
-    }
-    
-    cuda_status = cudaMalloc(&d_c, bytes);
-    if (cuda_status != cudaSuccess) {
-        cudaFree(d_a);
-        cudaFree(d_b);
-        throw std::runtime_error("cudaMalloc failed for d_c");
-    }
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
 
     cudaStream_t stream;
     cudaStreamCreate(&stream);
@@ -56,24 +32,22 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a,
     const float beta = 0.0f;
 
     cublasSgemm(handle,
-                CUBLAS_OP_N, 
-                CUBLAS_OP_N,
+                CUBLAS_OP_N, CUBLAS_OP_N,
                 n, n, n,
                 &alpha,
+                d_b, n,
                 d_a, n,
-                d_b, n,  
                 &beta,
                 d_c, n);
 
     cudaMemcpyAsync(c.data(), d_c, bytes, cudaMemcpyDeviceToHost, stream);
-
     cudaStreamSynchronize(stream);
 
     cublasDestroy(handle);
-    cudaStreamDestroy(stream);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+    cudaStreamDestroy(stream);
 
     return c;
 }
