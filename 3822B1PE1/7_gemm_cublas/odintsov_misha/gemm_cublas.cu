f29aee4c8461775f0cf8aea7fd21a1086c23c323
rerard873 <odincovmisa16@gmail.com>
2025-11-13 13:35:06 +0300

    try fix task 7

--- 3822B1PE1/7_gemm_cublas/odintsov_misha/gemm_cublas.cu
+++ 3822B1PE1/7_gemm_cublas/odintsov_misha/gemm_cublas.cu
@@ -1,14 +1,18 @@
 #include "gemm_cublas.h"
 #include <vector>
-#include <cublas_v2.h>
+#include <cstring> 
+
+// CUDA и CUBLAS
 #include <cuda_runtime.h>
-#include <iostream>
+#include <cublas_v2.h>
+
+
 
 std::vector<float> GemmCUBLAS(const std::vector<float>& a, const std::vector<float>& b, int n) {
     size_t len = n * n;
-
     std::vector<float> output(len, 0.0f);
 
+    // Выделение памяти на устройстве
     float* d_a;
     float* d_b;
     float* d_c;
@@ -17,29 +21,40 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a, const std::vector<flo
     cudaMalloc((void**)&d_b, len * sizeof(float));
     cudaMalloc((void**)&d_c, len * sizeof(float));
 
-    cudaStream_t stream;
-    cudaStreamCreate(&stream);
+    // Использование фиксированной памяти на хосте
+    float* h_a;
+    float* h_b;
+    cudaMallocHost((void**)&h_a, len * sizeof(float));
+    cudaMallocHost((void**)&h_b, len * sizeof(float));
 
-    cudaMemcpyAsync(d_a, a.data(), len * sizeof(float), cudaMemcpyHostToDevice, stream);
-    cudaMemcpyAsync(d_b, b.data(), len * sizeof(float), cudaMemcpyHostToDevice, stream);
+    // Копирование данных из std::vector в зафиксированную память
+    std::memcpy(h_a, a.data(), len * sizeof(float));
+    std::memcpy(h_b, b.data(), len * sizeof(float));
 
+    // Копирование данных в память устройства
+    cudaMemcpy(d_a, h_a, len * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, len * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Инициализация CUBLAS
     cublasHandle_t handle;
     cublasCreate(&handle);
 
     const float alpha = 1.0f;
     const float beta = 0.0f;
 
-    cudaStreamSynchronize(stream);  
+    // Выполнение матричного умножения
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, n, n, n, &alpha, d_a, n, d_b, n, &beta, d_c, n);
-    cudaMemcpyAsync(output.data(), d_c, len * sizeof(float), cudaMemcpyDeviceToHost, stream);
 
-    cudaStreamSynchronize(stream);
+    // Копирование результата обратно на хост
+    cudaMemcpy(output.data(), d_c, len * sizeof(float), cudaMemcpyDeviceToHost);
 
+    // Освобождение памяти
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+    cudaFreeHost(h_a);
+    cudaFreeHost(h_b);
     cublasDestroy(handle);
-    cudaStreamDestroy(stream);
 
     return output;
 }
