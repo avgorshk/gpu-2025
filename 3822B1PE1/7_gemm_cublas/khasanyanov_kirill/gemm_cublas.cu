ed83443f9618f842e489c45f3c91941981d050b4
khasanyanovk <hasanyanovk2@mail.ru>
2025-11-13 19:22:51 +0300

    complete task 7

---
+++ 3822B1PE1/7_gemm_cublas/khasanyanov_kirill/gemm_cublas.cu
@@ -0,0 +1,44 @@
+#include "gemm_cublas.h"
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+std::vector<float> GemmCUBLAS(const std::vector<float> &a,
+                              const std::vector<float> &b, int n) {
+  if (n == 0) {
+    return std::vector<float>();
+  }
+
+  std::vector<float> c(n * n);
+
+  float *d_a = nullptr;
+  float *d_b = nullptr;
+  float *d_c = nullptr;
+
+  size_t bytes = n * n * sizeof(float);
+
+  cudaMalloc(&d_a, bytes);
+  cudaMalloc(&d_b, bytes);
+  cudaMalloc(&d_c, bytes);
+
+  cudaMemcpy(d_a, a.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, b.data(), bytes, cudaMemcpyHostToDevice);
+
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  const float alpha = 1.0f;
+  const float beta = 0.0f;
+
+  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n, &alpha, d_b, n, d_a, n,
+              &beta, d_c, n);
+
+  cublasDestroy(handle);
+
+  cudaMemcpy(c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+
+  return c;
+}
