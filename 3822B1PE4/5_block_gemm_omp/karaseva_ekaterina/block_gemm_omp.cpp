2fb1cd0c968d6b7865755a9691337876711ded53
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-11-16 09:11:39 +0300

    5_block_gemm_omp task

---
+++ 3822B1PE4/5_block_gemm_omp/karaseva_ekaterina/block_gemm_omp.cpp
@@ -0,0 +1,7 @@
+#include "block_gemm_omp.h"
+
+std::vector<float> BlockGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+    // Place your implementation here
+}
