e9196bda011776795a203292f94713683b81a8c8
VlJulia <j.v.04@bk.ru>
2025-11-30 05:01:50 +0300

    formula changed

--- 3822B1PE2/1_gelu_omp/vladimirova_julia/gelu_omp.cpp
+++ 3822B1PE2/1_gelu_omp/vladimirova_julia/gelu_omp.cpp
@@ -3,9 +3,12 @@
 std::vector<float> GeluOMP(const std::vector<float>& input) {
     size_t vector_size = input.size();
     std::vector<float> output(input);
+    float ct_1 = sqrt(2.0f/acos(-1.0f));
+    float ct_2 = 0.044715f;
 #pragma omp parallel for
     for (int i = 0; i < vector_size; i++) {
-        output[i]/=(1.0f + exp(-1.701744 * input[i]));
+        float x = input[i];
+        output[i] = x * 0.5f * (1.0f + tanh(ct_1 * x * (1.0f + ct_2 * x * x)));
     }
     return output;
 }
