2bc08f902078000aaaa4631cbaefff4485ce16d1
belovtut <artem.belov2004@yandex.ru>
2025-12-05 03:13:29 +0300

    update: task3 done

---
+++ 3822B1PE1/3_naive_gemm_omp/belov_artem/naive_gemm_omp.cpp
@@ -0,0 +1,20 @@
+#include "naive_gemm_omp.h"
+#include <omp.h>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+    std::vector<float> c(n * n, 0.0f);
+    
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            const float a_ik = a[i * n + k];
+            for (int j = 0; j < n; ++j) {
+                c[i * n + j] += a_ik * b[k * n + j];
+            }
+        }
+    }
+    
+    return c;
+}
