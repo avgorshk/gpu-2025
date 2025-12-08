3c4dc8be738f6a2294b34e5da888aeceaf7bf8a0
Egor1dzeN <egormorozov123450@gmail.com>
2025-12-08 13:50:27 +0300

    7 lab fix

--- 3822B1PE1/7_gemm_cublas/morozov_egor/gemm_cublas.cu
+++ 3822B1PE1/7_gemm_cublas/morozov_egor/gemm_cublas.cu
@@ -2,65 +2,42 @@
 #include "gemm_cublas.h"
 #include <iostream>
 
-#define CUBLAS_CHECK(call) \
-    do { \
-        cublasStatus_t status = call; \
-        if (status != CUBLAS_STATUS_SUCCESS) { \
-            std::cerr << "CUBLAS error at " << __FILE__ << ":" << __LINE__ << ": " \
-                      << status << std::endl; \
-            exit(EXIT_FAILURE); \
-        } \
-    } while(0)
-
-#define CUDA_CHECK(call) \
-    do { \
-        cudaError_t err = call; \
-        if (err != cudaSuccess) { \
-            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__ << ": " \
-                      << cudaGetErrorString(err) << std::endl; \
-            exit(EXIT_FAILURE); \
-        } \
-    } while(0)
-
 std::vector<float> GemmCUBLAS(const std::vector<float> &a,
                               const std::vector<float> &b,
                               int n) {
     cublasHandle_t handle;
-    CUBLAS_CHECK(cublasCreate(&handle));
+    cublasCreate(&handle);
 
     size_t size = n * n * sizeof(float);
 
     float *d_A;
     float *d_B;
     float *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
 
-    CUDA_CHECK(cudaMalloc(&d_A, size));
-    CUDA_CHECK(cudaMalloc(&d_B, size));
-    CUDA_CHECK(cudaMalloc(&d_C, size));
-
-    CUDA_CHECK(cudaMemcpy(d_A, a.data(), size, cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(d_B, b.data(), size, cudaMemcpyHostToDevice));
+    cudaMemcpy(d_A, a.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, b.data(), size, cudaMemcpyHostToDevice);
 
     const float alpha = 1.0f;
     const float beta = 0.0f;
-
-    CUBLAS_CHECK(cublasSgemm(handle,
-                             CUBLAS_OP_N,
-                             CUBLAS_OP_N,
-                             n, n, n,
-                             &alpha,
-                             d_A, n,
-                             d_B, n,
-                             &beta,
-                             d_C, n));
+    cublasSgemm(handle,
+                CUBLAS_OP_N, CUBLAS_OP_N,
+                n, n, n,
+                &alpha,
+                d_B, n,
+                d_A, n,
+                &beta,
+                d_C, n);
 
     std::vector<float> result(n * n);
-    CUDA_CHECK(cudaMemcpy(result.data(), d_C, size, cudaMemcpyDeviceToHost));
+    cudaMemcpy(result.data(), d_C, size, cudaMemcpyDeviceToHost);
 
-    CUDA_CHECK(cudaFree(d_A));
-    CUDA_CHECK(cudaFree(d_B));
-    CUDA_CHECK(cudaFree(d_C));
-    CUBLAS_CHECK(cublasDestroy(handle));
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    cublasDestroy(handle);
 
     return result;
 }
