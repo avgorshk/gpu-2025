ffd661ecdf9825e9f98404038980d022e3b4d343
markinivan <markinivan2004@yandex.ru>
2025-12-15 03:43:43 +0300

    Lab1

--- 3822B1FI2/1_gelu_omp/markin_ivan/gelu_omp.cpp
+++ 3822B1FI2/1_gelu_omp/markin_ivan/gelu_omp.cpp
@@ -1,22 +1,28 @@
 #include "gelu_omp.h"
 #include <cmath>
 #include <vector>
-#include <omp.h>
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
-    const size_t n = input.size();
-    std::vector<float> result(n);
+    constexpr float PI_CONST = 3.14159265358979323846f;
+    constexpr float SQRT_2_OVER_PI = 0.79788458347320556640625f;
+    constexpr float GELU_COEFF = 0.044715f;
 
-    if (n == 0) return result;
-    
-    const float* in = input.data();
-    float* out = result.data();
-    
-    #pragma omp parallel for
-    for (size_t i = 0; i < n; ++i) {
-        const float x = in[i];
-        const float t = 0.7978845608028654f * x + 0.035677f * x * x * x;
-        out[i] = 0.5f * x * (1.0f + t * (27.0f + t*t) / (27.0f + 9.0f * t*t));
+    std::vector<float> output(input.size());
+    const int total_elements = static_cast<int>(input.size());
+
+#pragma omp parallel for schedule(static)
+    for (int i = 0; i < total_elements; ++i) {
+        float x = input[i];
+        float x_cubed = x * x * x;
+
+        float inner_expr = SQRT_2_OVER_PI * (x + GELU_COEFF * x_cubed);
+
+        float exp_positive = std::exp(inner_expr);
+        float exp_negative = std::exp(-inner_expr);
+        float tanh_approx = (exp_positive - exp_negative) / (exp_positive + exp_negative);
+
+        output[i] = 0.5f * x * (1.0f + tanh_approx);
     }
-    return result;
+
+    return output;
 }
