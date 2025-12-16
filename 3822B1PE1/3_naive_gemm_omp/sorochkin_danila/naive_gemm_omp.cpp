fa30751fe408e92924e279effc91a9a9cd295921
dssss12 <125500790+dssss12@users.noreply.github.com>
2025-12-16 22:23:56 +0300

    task3

---
+++ 3822B1PE1/3_naive_gemm_omp/sorochkin_danila/naive_gemm_omp.cpp
@@ -0,0 +1,27 @@
+﻿#include "naive_gemm_omp.h"
+#include <omp.h>
+#include <vector>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+    const std::vector<float>& b,
+    int n) {
+    std::vector<float> c(n * n, 0.0f); 
+
+
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float sum = 0.0f;
+
+            
+            #pragma omp simd
+            for (int k = 0; k < n; ++k) {
+                sum += a[i * n + k] * b[k * n + j];
+            }
+
+            c[i * n + j] = sum;
+        }
+    }
+
+    return c;
+}
