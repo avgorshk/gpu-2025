41fd9d3c48c58d07daaee47427fc925e5e3671d6
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-20 19:51:18 +0300

    Add BlockGemmOMP function for OpenMP parallelized GEMM computation

---
+++ 3822B1PE3/5_block_gemm_omp/chernykh_andrey/block_gemm_omp.cpp
@@ -0,0 +1,39 @@
+#include "block_gemm_omp.h"
+#include <vector>
+#include <omp.h>
+
+
+std::vector<float> BlockGemmOMP(
+    const std::vector<float> &a,
+    const std::vector<float> &b,
+    int n
+) {
+    if (n <= 0) {
+        return {};
+    }
+    constexpr int BLOCK_SIZE = 64;
+
+    std::vector<float> c(n * n, 0.0f);
+
+#pragma omp parallel for schedule(static)
+    for (int bi = 0; bi < n; bi += BLOCK_SIZE) {
+        for (int bj = 0; bj < n; bj += BLOCK_SIZE) {
+            for (int bk = 0; bk < n; bk += BLOCK_SIZE) {
+                for (int i = bi; i < bi + BLOCK_SIZE; ++i) {
+                    const float *a_row_ptr = &a[i * n];
+                    float *c_row_ptr = &c[i * n];
+                    for (int k = bk; k < bk + BLOCK_SIZE; ++k) {
+                        float a_item = a_row_ptr[k];
+                        const float *b_row_ptr = &b[k * n];
+#pragma omp simd
+                        for (int j = bj; j < bj + BLOCK_SIZE; ++j) {
+                            c_row_ptr[j] += a_item * b_row_ptr[j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return c;
+}
