a23ca1e6d2cf46f7eda11011fd0c2b0be892df41
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-21 12:49:54 +0300

    Add FffCUFFT function for CUDA-based FFT computation

---
+++ 3822B1PE3/8_fft_cufft/chernykh_andrey/fft_cufft.cu
@@ -0,0 +1,41 @@
+#include "fft_cufft.h"
+#include <vector>
+#include <cuda_runtime.h>
+#include <cufft.h>
+
+__global__ void NormalizeKernel(float *data, size_t n, float factor) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < n) {
+        data[index] /= factor;
+    }
+}
+
+std::vector<float> FffCUFFT(const std::vector<float> &input, int batch) {
+    if (input.empty() || batch <= 0) {
+        return {};
+    }
+
+    size_t size = input.size();
+    size_t data_size = size * sizeof(float);
+    auto signal_length = static_cast<int>(size / (2 * batch));
+    std::vector<float> output(size);
+
+    size_t block_size = 256;
+    size_t num_blocks = (size + block_size - 1) / block_size;
+
+    float *d_data = nullptr;
+    cudaMalloc(&d_data, data_size);
+
+    cudaMemcpy(d_data, input.data(), data_size, cudaMemcpyHostToDevice);
+    cufftHandle plan;
+    cufftPlan1d(&plan, signal_length, CUFFT_C2C, batch);
+    cufftExecC2C(plan, (cufftComplex *) d_data, (cufftComplex *) d_data, CUFFT_FORWARD);
+    cufftExecC2C(plan, (cufftComplex *) d_data, (cufftComplex *) d_data, CUFFT_INVERSE);
+    NormalizeKernel<<<num_blocks, block_size>>>(d_data, size, signal_length);
+    cudaDeviceSynchronize();
+    cudaMemcpy(output.data(), d_data, data_size, cudaMemcpyDeviceToHost);
+
+    cufftDestroy(plan);
+    cudaFree(d_data);
+    return output;
+}
