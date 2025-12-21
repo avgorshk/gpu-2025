8004c4b1b0eb3ed0e26e1961538f14ffd3149b27
Dmitry Kavtorev <133030437+kavtorevd@users.noreply.github.com>
2025-12-21 14:13:41 +0300

    fix: task 7

--- 3822B1PE2/7_gemm_cublas/kavtorev_dmitry/gemm_cublas.cu
+++ 3822B1PE2/7_gemm_cublas/kavtorev_dmitry/gemm_cublas.cu
@@ -6,50 +6,38 @@
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
                               const std::vector<float>& b,
                               int n) {
-    size_t size = n * n * sizeof(float);
+    const size_t size = n * n * sizeof(float);
     std::vector<float> c(n * n);
-    
-    float *d_a, *d_b, *d_c;
-    cudaMalloc(&d_a, size);
-    cudaMalloc(&d_b, size);
-    cudaMalloc(&d_c, size);
-    
-    std::vector<float> a_transposed(n * n);
-    std::vector<float> b_transposed(n * n);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            a_transposed[j * n + i] = a[i * n + j];
-            b_transposed[j * n + i] = b[i * n + j];
-        }
-    }
-    
-    cudaMemcpy(d_a, a_transposed.data(), size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b_transposed.data(), size, cudaMemcpyHostToDevice);
-    
+
     cublasHandle_t handle;
     cublasCreate(&handle);
-    
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+    cublasSetStream(handle, stream);
+
+    cudaMemcpyAsync(d_A, a.data(), size, cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_B, b.data(), size, cudaMemcpyHostToDevice, stream);
+
     const float alpha = 1.0f;
     const float beta = 0.0f;
-    
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n,
-                &alpha, d_b, n, d_a, n, &beta, d_c, n);
-    
-    cudaDeviceSynchronize();
-    
-    std::vector<float> c_transposed(n * n);
-    cudaMemcpy(c_transposed.data(), d_c, size, cudaMemcpyDeviceToHost);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            c[i * n + j] = c_transposed[j * n + i];
-        }
-    }
-    
+                &alpha, d_B, n, d_A, n, &beta, d_C, n);
+
+    cudaMemcpyAsync(c.data(), d_C, size, cudaMemcpyDeviceToHost, stream);
+    cudaStreamSynchronize(stream);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    cudaStreamDestroy(stream);
     cublasDestroy(handle);
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-    
+
     return c;
 }
 
