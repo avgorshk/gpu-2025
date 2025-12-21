e708dc0abe12306f97373c4cb31aa4436d74cc52
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-21 14:16:37 +0300

    Optimize BlockGemmKernel for improved memory access patterns

--- 3822B1PE3/6_block_gemm_cuda/chernykh_andrey/block_gemm_cuda.cu
+++ 3822B1PE3/6_block_gemm_cuda/chernykh_andrey/block_gemm_cuda.cu
@@ -3,7 +3,6 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-
 constexpr int BLOCK_SIZE = 32;
 
 __global__ void BlockGemmKernel(
@@ -21,10 +20,22 @@ __global__ void BlockGemmKernel(
     __shared__ float tile_b[BLOCK_SIZE][BLOCK_SIZE];
 
     float sum = 0.0f;
-    for (int i = 0; i < n / BLOCK_SIZE; i++) {
-        tile_a[local_row][local_col] = a[global_row * n + (i * BLOCK_SIZE + local_col)];
-        tile_b[local_row][local_col] = b[(i * BLOCK_SIZE + local_row) * n + global_col];
+    int num_tiles = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    for (int i = 0; i < num_tiles; i++) {
+        int a_col = i * BLOCK_SIZE + local_col;
+        if (global_row < n && a_col < n) {
+            tile_a[local_row][local_col] = a[global_row * n + a_col];
+        } else {
+            tile_a[local_row][local_col] = 0.0f;
+        }
+        int b_row = i * BLOCK_SIZE + local_row;
+        if (b_row < n && global_col < n) {
+            tile_b[local_row][local_col] = b[b_row * n + global_col];
+        } else {
+            tile_b[local_row][local_col] = 0.0f;
+        }
         __syncthreads();
+
 #pragma unroll
         for (int j = 0; j < BLOCK_SIZE; j++) {
             sum += tile_a[local_row][j] * tile_b[j][local_col];
@@ -48,7 +59,11 @@ std::vector<float> BlockGemmCUDA(
 
     size_t size = n * n;
     size_t data_size = size * sizeof(float);
-    std::vector<float> c(size);
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid(
+        (n + block.x - 1) / block.x,
+        (n + block.y - 1) / block.y
+    );
 
     float *d_a = nullptr;
     cudaMalloc(&d_a, data_size);
@@ -59,12 +74,8 @@ std::vector<float> BlockGemmCUDA(
 
     cudaMemcpy(d_a, a.data(), data_size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b.data(), data_size, cudaMemcpyHostToDevice);
-    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 grid(
-        (n + block.x - 1) / block.x,
-        (n + block.y - 1) / block.y
-    );
     BlockGemmKernel<<<grid, block>>>(d_a, d_b, d_c, n);
+    std::vector<float> c(size);
     cudaDeviceSynchronize();
     cudaMemcpy(c.data(), d_c, data_size, cudaMemcpyDeviceToHost);
 
