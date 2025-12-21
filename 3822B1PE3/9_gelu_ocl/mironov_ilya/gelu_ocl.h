4ccbf774961eae5bebb53785dd40f6ad45c6b7c0
ilya <mironovvIlya2003@gmail.com>
2025-12-21 15:06:13 +0300

    first

---
+++ 3822B1PE3/9_gelu_ocl/mironov_ilya/gelu_ocl.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 Mironov-Ilya
+#ifndef __GELU_OCL_H
+#define __GELU_OCL_H
+
+#include <vector>
+
+std::vector<float> GeluOCL(const std::vector<float>& input, int platform);
+
+#endif  // __GELU_OCL_H
