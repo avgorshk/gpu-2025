f123ff6fa47c816db49153e5d9218d7c7e98e961
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-21 14:07:52 +0300

    Improve BlockGemmOMP by adjusting loop bounds for performance

--- 3822B1PE3/5_block_gemm_omp/chernykh_andrey/block_gemm_omp.cpp
+++ 3822B1PE3/5_block_gemm_omp/chernykh_andrey/block_gemm_omp.cpp
@@ -1,7 +1,7 @@
 #include "block_gemm_omp.h"
+#include <algorithm>
 #include <vector>
 #include <omp.h>
-
 
 std::vector<float> BlockGemmOMP(
     const std::vector<float> &a,
@@ -19,14 +19,20 @@ std::vector<float> BlockGemmOMP(
     for (int bi = 0; bi < n; bi += BLOCK_SIZE) {
         for (int bj = 0; bj < n; bj += BLOCK_SIZE) {
             for (int bk = 0; bk < n; bk += BLOCK_SIZE) {
-                for (int i = bi; i < bi + BLOCK_SIZE; ++i) {
+                int i_max = std::min(bi + BLOCK_SIZE, n);
+                int j_max = std::min(bj + BLOCK_SIZE, n);
+                int k_max = std::min(bk + BLOCK_SIZE, n);
+
+                for (int i = bi; i < i_max; ++i) {
                     const float *a_row_ptr = &a[i * n];
                     float *c_row_ptr = &c[i * n];
-                    for (int k = bk; k < bk + BLOCK_SIZE; ++k) {
+
+                    for (int k = bk; k < k_max; ++k) {
                         float a_item = a_row_ptr[k];
                         const float *b_row_ptr = &b[k * n];
+
 #pragma omp simd
-                        for (int j = bj; j < bj + BLOCK_SIZE; ++j) {
+                        for (int j = bj; j < j_max; ++j) {
                             c_row_ptr[j] += a_item * b_row_ptr[j];
                         }
                     }
