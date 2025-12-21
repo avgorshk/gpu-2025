2c729de06333d5c98794bf19036c45d738878812
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-21 13:56:18 +0300

    Optimize GeluKernel by replacing std::tanh with tanhf for performance

--- 3822B1PE3/2_gelu_cuda/chernykh_andrey/gelu_cuda.cu
+++ 3822B1PE3/2_gelu_cuda/chernykh_andrey/gelu_cuda.cu
@@ -1,8 +1,8 @@
 #include "gelu_cuda.h"
 #include <cmath>
+#include <vector>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-
 
 __global__ void GeluKernel(const float *input, float *output, size_t size) {
     constexpr float SQRT_2_PI = 0.7978845608f; // sqrt(2 / pi)
@@ -15,7 +15,7 @@ __global__ void GeluKernel(const float *input, float *output, size_t size) {
 
     float x = input[index];
     float arg = SQRT_2_PI * (x + COEF * x * x * x);
-    output[index] = 0.5f * x * (1.0f + std::tanh(arg));
+    output[index] = 0.5f * x * (1.0f + tanhf(arg));
 }
 
 std::vector<float> GeluCUDA(const std::vector<float> &input) {
@@ -25,8 +25,6 @@ std::vector<float> GeluCUDA(const std::vector<float> &input) {
 
     size_t size = input.size();
     size_t data_size = size * sizeof(float);
-    std::vector<float> output(size);
-
     size_t block_size = 256;
     size_t num_blocks = (size + block_size - 1) / block_size;
 
@@ -37,6 +35,7 @@ std::vector<float> GeluCUDA(const std::vector<float> &input) {
 
     cudaMemcpy(d_input, input.data(), data_size, cudaMemcpyHostToDevice);
     GeluKernel<<<num_blocks, block_size>>>(d_input, d_output, size);
+    std::vector<float> output(size);
     cudaDeviceSynchronize();
     cudaMemcpy(output.data(), d_output, data_size, cudaMemcpyDeviceToHost);
 
