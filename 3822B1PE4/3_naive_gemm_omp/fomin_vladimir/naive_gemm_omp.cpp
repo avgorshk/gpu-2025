1a1e3437ef75e68410aac86253048296626f9a96
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-12-22 20:51:12 +0300

    task 3

---
+++ 3822B1PE4/3_naive_gemm_omp/fomin_vladimir/naive_gemm_omp.cpp
@@ -0,0 +1,23 @@
+#include "naive_gemm_omp.h"
+#include <vector>
+#include <omp.h>
+#include <cstring>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+    std::vector<float> c(n * n, 0.0f);
+
+    #pragma omp parallel for default(none) shared(a, b, c, n) schedule(static)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < n; ++k) {
+                sum += a[i * n + k] * b[k * n + j];
+            }
+            c[i * n + j] = sum;
+        }
+    }
+
+    return c;
+}
