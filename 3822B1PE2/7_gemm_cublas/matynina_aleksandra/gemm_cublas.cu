4c0c320f2d0118d39503d60f334c666ac609883e
Aleksandra Matyunina <ERTY-00@yandex.ru>
2025-12-25 20:37:54 +0300

    FIX task 7

--- 3822B1PE2/7_gemm_cublas/matynina_aleksandra/gemm_cublas.cu
+++ 3822B1PE2/7_gemm_cublas/matynina_aleksandra/gemm_cublas.cu
@@ -10,115 +10,97 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a,
   throw std::invalid_argument("Matrix size mismatch");
  }
 
- cublasHandle_t handle;
- cublasStatus_t status = cublasCreate(&handle);
- if (status != CUBLAS_STATUS_SUCCESS) {
-  throw std::runtime_error("Failed to initialize cuBLAS");
- }
-
- float *d_a, *d_b, *d_c;
+ std::vector<float> c(n * n);
+ 
+ float *d_A, *d_B, *d_C;
  size_t size = n * n * sizeof(float);
  
  cudaError_t err;
- err = cudaMalloc((void**)&d_a, size);
+ err = cudaMalloc((void**)&d_A, size);
  if (err != cudaSuccess) {
-  cublasDestroy(handle);
   throw std::runtime_error("Failed to allocate device memory for matrix A");
  }
  
- err = cudaMalloc((void**)&d_b, size);
+ err = cudaMalloc((void**)&d_B, size);
  if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cublasDestroy(handle);
+  cudaFree(d_A);
   throw std::runtime_error("Failed to allocate device memory for matrix B");
  }
  
- err = cudaMalloc((void**)&d_c, size);
+ err = cudaMalloc((void**)&d_C, size);
  if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cublasDestroy(handle);
+  cudaFree(d_A);
+  cudaFree(d_B);
   throw std::runtime_error("Failed to allocate device memory for matrix C");
  }
 
- err = cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice);
- if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
+ cublasHandle_t handle;
+ cublasStatus_t status = cublasCreate(&handle);
+ if (status != CUBLAS_STATUS_SUCCESS) {
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+  throw std::runtime_error("Failed to initialize cuBLAS");
+ }
+
+ status = cublasSetMatrix(n, n, sizeof(float), a.data(), n, d_A, n);
+ if (status != CUBLAS_STATUS_SUCCESS) {
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
   cublasDestroy(handle);
   throw std::runtime_error("Failed to copy matrix A to device");
  }
- 
- err = cudaMemcpy(d_b, b.data(), size, cudaMemcpyHostToDevice);
- if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
+
+ status = cublasSetMatrix(n, n, sizeof(float), b.data(), n, d_B, n);
+ if (status != CUBLAS_STATUS_SUCCESS) {
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
   cublasDestroy(handle);
   throw std::runtime_error("Failed to copy matrix B to device");
  }
 
- float *h_result;
- err = cudaMallocHost((void**)&h_result, size);
- if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
-  cublasDestroy(handle);
-  throw std::runtime_error("Failed to allocate pinned host memory");
- }
-
  const float alpha = 1.0f;
  const float beta = 0.0f;
- 
+
  status = cublasSgemm(handle,
-                      CUBLAS_OP_T,
-                      CUBLAS_OP_T,
+                      CUBLAS_OP_N,
+                      CUBLAS_OP_N,
                       n,
                       n,
                       n,
                       &alpha,
-                      d_b,
+                      d_B,
                       n,
-                      d_a,
+                      d_A,
                       n,
                       &beta,
-                      d_c,
+                      d_C,
                       n);
  
  if (status != CUBLAS_STATUS_SUCCESS) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
-  cudaFreeHost(h_result);
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
   cublasDestroy(handle);
   throw std::runtime_error("cuBLAS matrix multiplication failed");
  }
 
- err = cudaMemcpy(h_result, d_c, size, cudaMemcpyDeviceToHost);
- if (err != cudaSuccess) {
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
-  cudaFreeHost(h_result);
+ status = cublasGetMatrix(n, n, sizeof(float), d_C, n, c.data(), n);
+ if (status != CUBLAS_STATUS_SUCCESS) {
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
   cublasDestroy(handle);
   throw std::runtime_error("Failed to copy result from device");
  }
 
- cudaFree(d_a);
- cudaFree(d_b);
- cudaFree(d_c);
  cublasDestroy(handle);
-
- std::vector<float> result_row_major(n * n);
- for (int i = 0; i < n; i++) {
-  for (int j = 0; j < n; j++) {
-   result_row_major[i * n + j] = h_result[j * n + i];
-  }
- }
-
- cudaFreeHost(h_result);
- return result_row_major;
+ cudaFree(d_A);
+ cudaFree(d_B);
+ cudaFree(d_C);
+ 
+ return c;
 }
 
